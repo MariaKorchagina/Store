06718fededebf783bd171b95d0b55b5e29eaa213
Maria Korchagina <kobysheva_maria@mail.ru>
2023-12-01 13:16:58 +0300

    added redux

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,9 +3,17 @@ import AppRoutes from "../Routes/Routes";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Sidebar from "../Sidebar/Sidebar";
-
+import { useDispatch } from "react-redux";
+import { getCategories } from "../../features/categories/categoriesSlice";
+import { useEffect } from "react";
 
 const App = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getCategories());
+    }, [dispatch])
+
     return (
         <div className="app">
             <Header />
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import styles from '../../styles/Sidebar.module.css';
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
+    const { list } = useSelector(({ categories }) => categories)
     return (
         <section className={styles.sidebar}>
             <div className={styles.title}>CATEGORIES</div>
             <nav>
                 <ul className={styles.menu}>
-                    <li>
-                        <NavLink to={`/categories/${1}`}>
-                            Link
-                        </NavLink>
-                    </li>
+                    {list.map(({ id, name }) => (
+                        <li key={id}>
+                            <NavLink
+                                className={({ isActive }) =>
+                                    `${styles.link} ${isActive ? styles.active : ""}`
+                                }
+                                to={`/categories/${id}`}>
+                                {name}
+                            </NavLink>
+                        </li>
+                    ))}
                 </ul>
             </nav>
             <div className={styles.footer}>
